840f514fba6f1c2eadcc41f495383a2f539bc056
1993heqiang <531364804@qq.com>
2017-01-23 17:56:41 +0800

    流测试

--- src/main/java/com/heqaing/test/Test_clazz.java
+++ src/main/java/com/heqaing/test/Test_clazz.java
@@ -1,11 +1,27 @@
 package com.heqaing.test;
 
-import java.util.HashMap;
+import java.io.*;
 
 public class Test_clazz {
 
-	public static void main(String[] args) {
-		int[][] points = {{0,0},{0,1},{0,-1},{1,0},{-1,0}};
+	public static void main(String[] args) throws IOException {
+			try {
+				BufferedInputStream bis = new BufferedInputStream(new FileInputStream("D:\\test\\origin.mkv"));
+				BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("D:\\test\\cope2.mkv"));
+				byte[] bytes = new byte[1870];
+				long t1 = System.currentTimeMillis();
+				int i;
+				while ((i=bis.read(bytes,0,1870))!=-1){
+					bos.write(bytes,0,i);
+				}
+				long t2 = System.currentTimeMillis();
+				System.out.println(t2-t1);
+				bis.close();
+				bos.close();
+			} catch (IOException e) {
+				System.err.println("FileStreamsTest: " + e);
+			}
+
 
 	}
 }
