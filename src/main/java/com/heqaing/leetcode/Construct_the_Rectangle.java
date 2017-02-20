133323bca48eb1c0acf9d5e20093d84130c13d8f
1993heqiang <531364804@qq.com>
2017-02-20 14:38:07 +0800

    492. Construct the Rectangle

---
+++ src/main/java/com/heqaing/leetcode/Construct_the_Rectangle.java
@@ -0,0 +1,26 @@
+package com.heqaing.leetcode;
+
+import java.util.Arrays;
+
+public class Construct_the_Rectangle {
+
+	public static void main(String[] args) {
+		int area = 3;
+		long t1 = System.nanoTime();
+		int[] ret = constructRectangle(area);
+		long t2 = System.nanoTime();
+		System.out.println(t2-t1);
+		System.out.println(Arrays.toString(ret));
+	}
+	public static int[] constructRectangle(int area) {
+		int a = (int)Math.sqrt(area);
+		int[] ret = new int[2];
+		while(area%a != 0){
+            a--;
+        }
+		int b = area/a;
+        ret[0] = b;
+        ret[1] = a;
+        return ret;
+    }
+}
