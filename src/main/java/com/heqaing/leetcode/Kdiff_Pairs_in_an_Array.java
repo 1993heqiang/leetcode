0f3219b1d7e290fa2db6b70ddb33c6617ed1196b
1993heqiang <531364804@qq.com>
2017-03-23 14:53:09 +0800

    532. K-diff Pairs in an Array

---
+++ src/main/java/com/heqaing/leetcode/Kdiff_Pairs_in_an_Array.java
@@ -0,0 +1,44 @@
+package com.heqaing.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2017/3/23 0023.
+ */
+public class Kdiff_Pairs_in_an_Array {
+    public static void main(String[] args){
+        int[] nums = {3, 1, 4, 1, 5};
+        int k = 2;
+        long t1 = System.nanoTime();
+        int ret = findPairs(nums,k);
+        long t2 = System.nanoTime();
+        System.out.println(t2-t1);
+        System.out.println(ret);
+    }
+    public static int findPairs(int[] nums, int k) {
+        if (nums == null || nums.length == 0 || k < 0)   return 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+        for (int i : nums) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (k == 0) {
+                //count how many elements in the array that appear more than twice.
+                if (entry.getValue() >= 2) {
+                    count++;
+                }
+            } else {
+                if (map.containsKey(entry.getKey() + k)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
