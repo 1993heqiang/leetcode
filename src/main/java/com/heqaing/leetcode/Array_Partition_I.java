d9834dc15ae1b68ff5a4627ccffd50fab02abf2d
1993heqiang <531364804@qq.com>
2017-06-02 16:11:30 +0800

    561. Array Partition I

---
+++ src/main/java/com/heqaing/leetcode/Array_Partition_I.java
@@ -0,0 +1,25 @@
+package com.heqaing.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Created by HeQiang on 2017-06-02 15:52.
+ */
+public class Array_Partition_I {
+    public static void main(String[] args){
+        int[] nums = {1,4,3,2};
+        long t1 = System.nanoTime();
+        int result = arrayPairSum(nums);
+        long t2 = System.nanoTime();
+        System.out.println(t2-t1);
+        System.out.println(result);
+    }
+    public static int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for(int i=0;i<nums.length;i+=2){
+            result += nums[i];
+        }
+        return result;
+    }
+}
