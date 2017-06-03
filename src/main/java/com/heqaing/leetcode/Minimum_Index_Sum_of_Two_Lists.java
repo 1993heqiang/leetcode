af15b9935b7ace029a15b15fd9c944e596e22322
heqiang <531364804@qq.com>
2017-06-03 21:34:17 +0800

    599. Minimum Index Sum of Two Lists

--- src/main/java/com/heqaing/leetcode/Minimum_Index_Sum_of_Two_Lists.java
+++ src/main/java/com/heqaing/leetcode/Minimum_Index_Sum_of_Two_Lists.java
@@ -1,6 +1,6 @@
 package com.heqaing.leetcode;
 
-import java.util.Arrays;
+import java.util.*;
 
 /**
  * Created by HeQiang on 2017-06-02 18:02.
@@ -16,6 +16,27 @@ public class Minimum_Index_Sum_of_Two_Lists {
         System.out.println(Arrays.toString(result));
     }
     public static String[] findRestaurant(String[] list1, String[] list2) {
-        return null;
+        String[] longList = list1.length>list2.length?list1:list2;
+        String[] shortList = list1.length>list2.length?list2:list1;
+        Map<String,Integer> map = new HashMap<>();
+        for(int i=0;i<longList.length;i++){
+            map.put(longList[i],i);
+        }
+        int[] sumResult = new int[shortList.length];
+        int minSum = list1.length+list2.length;
+        for(int i=0;i<shortList.length;i++){
+            if(map.containsKey(shortList[i])){
+                sumResult[i] = i+map.get(shortList[i]);
+                if(sumResult[i]<minSum) minSum = sumResult[i];
+            }else {
+                sumResult[i] = -1;
+            }
+        }
+        List<String> result = new ArrayList<>();
+        for (int i=0;i<sumResult.length;i++){
+            if(sumResult[i]==minSum) result.add(shortList[i]);
+        }
+        String[] a = new String[result.size()];
+        return result.toArray(a);
     }
 }
