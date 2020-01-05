1ca074a8af26a5ff007d3f5c681b42e9550512e2
HeQiang <531364804@qq.com>
2020-01-05 19:47:50 +0800

    593. Valid Square :优化

--- src/main/java/com/rookie/leetcode/Valid_Square.java
+++ src/main/java/com/rookie/leetcode/Valid_Square.java
@@ -9,25 +9,29 @@ public class Valid_Square {
     }
 
     public boolean validSquare(int[] p1, int[] p2, int[] p3, int[] p4) {
-        int distance12 = (p1[0] - p2[0]) * ((p1[0] - p2[0])) + (p1[1] - p2[1]) * ((p1[1] - p2[1]));
-        int distance23 = (p2[0] - p3[0]) * ((p2[0] - p3[0])) + (p2[1] - p3[1]) * ((p2[1] - p3[1]));
-        int distance13 = (p1[0] - p3[0]) * ((p1[0] - p3[0])) + (p1[1] - p3[1]) * ((p1[1] - p3[1]));
+        int distance12 = distance(p1, p2);
+        int distance23 = distance(p2, p3);
+        int distance13 = distance(p1, p3);
         if (distance12 == 0 || distance13 == 0 || distance23 == 0) {
             return false;
         }
         if (distance12 == distance23 + distance13 && distance23 == distance13) {
-            return isRightTriangle(p4, p1, p2, distance12);
+            return isEquilateralRightTriangle(p4, p1, p2, distance12);
         } else if (distance13 == distance12 + distance23 && distance12 == distance23) {
-            return isRightTriangle(p4, p1, p3, distance13);
+            return isEquilateralRightTriangle(p4, p1, p3, distance13);
         } else if (distance23 == distance12 + distance13 && distance12 == distance13) {
-            return isRightTriangle(p4, p2, p3, distance23);
+            return isEquilateralRightTriangle(p4, p2, p3, distance23);
         }
         return false;
     }
 
-    private boolean isRightTriangle(int[] p1, int[] p2, int[] p3, int target) {
-        int distance12 = (p1[0] - p2[0]) * ((p1[0] - p2[0])) + (p1[1] - p2[1]) * ((p1[1] - p2[1]));
-        int distance13 = (p1[0] - p3[0]) * ((p1[0] - p3[0])) + (p1[1] - p3[1]) * ((p1[1] - p3[1]));
+    private boolean isEquilateralRightTriangle(int[] p1, int[] p2, int[] p3, int target) {
+        int distance12 = distance(p1, p2);
+        int distance13 = distance(p1, p3);
         return distance12 == distance13 && target == distance12 + distance13;
+    }
+
+    private int distance(int[] p1, int[] p2) {
+        return (p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]);
     }
 }
