bed86ac30b35de231e33aa5833f3d439ddcada04
heqiang <531364804@qq.com>
2021-02-26 20:01:44 +0800

    129. Sum Root to Leaf Numbers

---
+++ src/main/java/leetcodetop/bytedance/SumRootToLeafNumbers.java
@@ -0,0 +1,42 @@
+package leetcodetop.bytedance;
+
+/**
+ * https://leetcode-cn.com/problems/sum-root-to-leaf-numbers/solution/qiu-gen-dao-xie-zi-jie-dian-shu-zi-zhi-he-by-leetc/
+ */
+public class SumRootToLeafNumbers {
+
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int prevSum) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = prevSum * 10 + root.val;
+        if (root.left == null && root.right == null) {
+            return sum;
+        } else {
+            return dfs(root.left, sum) + dfs(root.right, sum);
+        }
+    }
+
+    private static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
