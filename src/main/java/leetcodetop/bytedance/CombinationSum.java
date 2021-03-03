4697c7556d1fb04e584971e02249593a84f0bfa2
heqiang <531364804@qq.com>
2021-03-03 19:51:26 +0800

    优化

--- src/main/java/leetcodetop/bytedance/CombinationSum.java
+++ src/main/java/leetcodetop/bytedance/CombinationSum.java
@@ -8,8 +8,8 @@ import java.util.List;
  */
 public class CombinationSum {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        List<Integer> combine = new ArrayList<Integer>();
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> combine = new ArrayList<>();
         dfs(candidates, target, ans, combine, 0);
         return ans;
     }
@@ -19,7 +19,7 @@ public class CombinationSum {
             return;
         }
         if (target == 0) {
-            ans.add(new ArrayList<Integer>(combine));
+            ans.add(new ArrayList<>(combine));
             return;
         }
         // 直接跳过
